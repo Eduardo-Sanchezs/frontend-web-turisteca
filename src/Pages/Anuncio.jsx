8ba576aff68427c680cb7e00b2c2fd464dcb11ac
Eduardo-Sanchez <sanchez.sanchez.22188@itsmante.edu.mx>
2025-03-09 14:03:22 -0600

    se realizaron ajustes en los div del la pagina anuncio

--- src/Pages/Anuncio.jsx
+++ src/Pages/Anuncio.jsx
@@ -6,13 +6,13 @@ const Anuncio = () => {
             <div className="h-screen bg-[url('/BackGround.jpg')] bg-cover bg-center">
                 <div className=' h-screen w-full flex flex-col items-center justify-center mt-2.5'>
                     <div className='h-full w-full lg flex flex-row  justify-between'>
-                        <div className='flex w-full md:w-1/2 justify-center items-center'>
+                        <div className='flex w-full md:w-1/2 justify-center items-center '>
                             <img src="/Logoo.png" alt="" className='h-110' />
                         </div>
 
                         <div className='w-full md:w-1/2 flex justify-center items-center'>
                             <div className='w-11/12 h-11/12 flex justify-center items-center rounded-4xl' style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-                                <div className='w-9/10 h-9/10 justify-center items-center rounded-4xl space-y-23'>
+                                <div className='w-9/10 h-2/3 justify-center items-center rounded-4xl space-y-23'>
                                     <div className='w-full'>
                                         <h1 className='text-center text-5xl font-extrabold text-[#409223]'> ¿Ya Conoces Nuesta App? </h1>
                                         <p className='text-center'>
@@ -20,9 +20,13 @@ const Anuncio = () => {
                                         </p>
                                     </div>
 
-                                    <div className='w-full'>
+                                    <div className='flex flex-col w-full items-center justify-center'>
                                         <a href="https://play.google.com/store/" target="_blank" rel="noopener noreferrer">
-                                            <img src="/Playstore.png" alt="Descargar en Google Play" className="w-48 md:w-52" />
+                                            <img src="/Playstore.png" alt="Descargar en Google Play" className="w-48 md:w-52 " />
+                                        </a>
+                                        <br />
+                                        <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+                                            <img src="/Appstore.png" alt="Descargar en App Store" className="w-48 md:w-52" />
                                         </a>
                                     </div>
                                 </div>
